470b20c5a53a139cb33f216d57529ccdf1e73b36
Alexsandro Viana de Abreu <viana.abreu@outlook.com>
2021-11-29 11:12:09 +0100

    app with url and random, url is not working

--- app.py
+++ app.py
@@ -1,46 +1,39 @@
 import streamlit as st
-from litreview.data import main
-import random
+import requests
 import pandas as pd
 
+
+URL = 'https://litreviewdockerimage-z37yi6v7za-ew.a.run.app/predict'
 st.set_page_config(
     page_title="Automated Literature Review",  # => Quick reference - Streamlit
     page_icon="📚",
     layout="wide",  # wide
     initial_sidebar_state="auto")  # collapsed
 
-
 ## setup for sidebar ##
 sideb = st.sidebar
 sideb.image("images/logo.png", use_column_width=True)
 sideb.markdown(
     "<h1 style='text-align: center; color: #5D6D7E;'>Demo Day - Batch 735 - Berlin</h1>",
-    unsafe_allow_html=True
-)
+    unsafe_allow_html=True)
 sideb.write(
     "<h1 style='text-align: center; color: #5D6D7E; font-size: 13px;'>Claire Filtz, Felix Wohlleben</h1>",
-    unsafe_allow_html=True
-)
+    unsafe_allow_html=True)
 sideb.write(
     "<h1 style='text-align: center; color: #5D6D7E; font-size: 13px;'>Issa Al Barwani, Alex Viana</h1>",
-    unsafe_allow_html=True
-)
+    unsafe_allow_html=True)
 
 ## setup main ##
 st.markdown(
     "<h1 style='text-align: center; color: #5D6D7E;'>an automated literature review tool</h1>",
-    unsafe_allow_html=True
-)
+    unsafe_allow_html=True)
 ## setup search bar ##
-
 
 #dirname = os.path.dirname(__file__)
 #filename = os.path.join(dirname, 'raw_data/trimmed_arxiv_docs5000.csv')
 #
 filename = 'https://storage.googleapis.com/wagon-data-735-vianadeabreu/data/trimmed_arxiv_docs5000.csv'
 test_df = pd.read_csv(filename)
-
-
 
 #search bar
 #title = st.text_input('','search')
@@ -56,14 +49,15 @@ test_df = pd.read_csv(filename)
 #option = st.slider('Select number of related papers to show', 1, 10, 3)
 
 
-
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+
 def remote_css(url):
     st.markdown(f'<link href="{url}" rel="stylesheet">',
                 unsafe_allow_html=True)
+
 
 def icon(icon_name):
     st.markdown(f'<i class="material-icons">{icon_name}</i>',
@@ -75,15 +69,23 @@ remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
 
 #icon("search")
 input_user = st.text_input("", "search...")
-neighbors = st.number_input('',
+neighbors = st.number_input(
+    '',
     min_value=2,
     max_value=30,
 )
 button_clicked = st.button("OK")
 
+#params = {'user_input': input_user, 'neighbors': neighbors}
+
 if button_clicked:
+    #response = requests.get(URL, data=params)
+    #response.text
+    #prediction = response.json()
+    #prediction
+    #indices = prediction['indices']
     #result = main.run_main(input_user, neighbors)
-    indices = random.sample(range(0,test_df.shape[0]),k=neighbors)
+    indices = random.sample(range(0, test_df.shape[0]), k=neighbors)
     st.write(
         f"<h1 style='text-align: left; color: #5D6D7E; font-size: 18px;'>Sorry, we are under construction! But here are {neighbors} nice papers to read:</h1>",
         unsafe_allow_html=True)
@@ -92,8 +94,8 @@ if button_clicked:
     #    unsafe_allow_html=True)
     for i in indices:
         st.write(
-                f"<h1 style='text-align: left; color: #ABB2B9; font-size: 15px;'>{test_df.iloc[i,2]}</h1>",
-                unsafe_allow_html=True)
+            f"<h1 style='text-align: left; color: #ABB2B9; font-size: 15px;'>{test_df.iloc[i,2]}</h1>",
+            unsafe_allow_html=True)
         st.write(
-               f"<h1 style='text-align: left; color: #ABB2B9; font-size: 15px; font-style: italic;'>{test_df.iloc[i,5]}</h1>",
-               unsafe_allow_html=True)
+            f"<h1 style='text-align: left; color: #ABB2B9; font-size: 15px; font-style: italic;'>{test_df.iloc[i,5]}</h1>",
+            unsafe_allow_html=True)
